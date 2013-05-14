cf56ab3e2c9945a02f9018f94527f94b67165a50
max96at <max96at@gmail.com>
2013-05-14 19:03:15 +0200

    Added main file.

---
+++ Class4Lua.lua
@@ -0,0 +1,58 @@
+#!/usr/bin/lua
+local args = {...}
+local path = args[1]
+
+local registry = setmetatable({ class = {}, object = {}, interface = {}},{__mode='k'})
+
+local construct
+
+local baseClass = {
+	__index = function(self, key)
+		if registry.class[self].vars[key].isPrivate then
+			error('Cannot access private variable.')
+		end
+		return registry.class[self].vars[key].value
+	end,
+
+	__newindex = function(self, key, val)
+		if registry.class[self].vars[key].isFinal then
+			error('Cannot modify final variable.')
+		else registry.class[self].vars[key].isPrivate then
+			error('Cannot access private variable.')
+		end
+		registry.class[self].vars[key].value = val
+	end,
+
+	__call = function(self, ...)
+		return construct(self)
+	end,
+
+	__tostring = function(self)
+		if registry.class[self].meta.tostring then
+			return registry.class[self].meta.tostring()
+		else
+			return 'class: <'..registry.class[self].system.addr..'>'
+		end
+	end,
+
+	__metatable = false
+}
+
+local metamath = {
+	'add',
+	'sub',
+	'mul',
+	'div',
+	'mod',
+	'pow',
+	'unm',
+	'concat',
+	'eq',
+	'lt',
+	'le'
+}
+for k,v in ipairs(metamath) do
+	baseClass['__'..v] = function(self, ...)
+		registry.class[self].meta[v](...)
+	end
+end
